b2b1256adddbd825434d6b50fe825c4aed3520b1
Elhabib TOUAOUA <bibahhabib8@gmail.com>
2022-12-19 15:09:22 +0100

    fixing name starting with . | space | space-

--- list_sort.py
+++ list_sort.py
@@ -3,6 +3,14 @@ import re
 lst_to_sort = ['1. Introduction', '10. Expense Tracker  Array Methods & Local Storage', '11. Music Player  HTML5 Audio API', '12. Infinite Scroll Posts  Fetch, AsyncAwait, CSS Loader', '13. Typing Game  DOM, Intervals, Events', '14. Speech Text Reader  Speech Synthesis', '15. Memory Cards  CSS Effects, Local Storage', '16. Lyrics Search App  Fetch, Pagination, Lyrics.ovh API', '17. Relaxer App  CSS Animations, setTimeout', '18. Breakout Game  HTML5 Canvas API', '19. New Year Countdown  DOM, Date & Time', '2. Form Validator  Intro Project', '20. Sortable List  Drag & Drop API', '21. Speak Number Guessing Game  Speech Recognition', '3. Movie Seat Booking  DOM & Local Storage', '4. Custom Video Player  HTML5 Video API', '5. Exchange Rate Calculator  Fetch & JSON Intro', 
 '6. DOM Array Methods  forEach, map, filter, sort, reduce', '7. Menu Slider & Modal  DOM & CSS', '8. Hangman Game  DOM, SVG, Events', '9. Meal Finder  Fetch & MealDB API']
 
+lst_to_sort2 = ['1 Introduction', '10 Expense Tracker  Array Methods & Local Storage', '11 Music Player  HTML5 Audio API', '12 Infinite Scroll Posts  Fetch, AsyncAwait, CSS Loader', '13 Typing Game  DOM, Intervals, Events', '14 Speech Text Reader  Speech Synthesis', '15 Memory Cards  CSS Effects, Local Storage', '16 Lyrics Search App  Fetch, Pagination, Lyrics.ovh API', '17 Relaxer App  CSS Animations, setTimeout', '18 Breakout Game  HTML5 Canvas API', '19 New Year Countdown  DOM, Date & Time', '2 Form Validator  Intro Project', '20 Sortable List  Drag & Drop API', '21 Speak Number Guessing Game  Speech Recognition', '3 Movie Seat Booking  DOM & Local Storage', '4 Custom Video Player  HTML5 Video API', '5 Exchange Rate Calculator  Fetch & JSON Intro', 
+'6 DOM Array Methods  forEach, map, filter, sort, reduce', '7 Menu Slider & Modal  DOM & CSS', '8 Hangman Game  DOM, SVG, Events', '9 Meal Finder  Fetch & MealDB API']
+
+lst_to_sort3 = ['1 - Introduction', '10 - Expense Tracker  Array Methods & Local Storage', '11 - Music Player  HTML5 Audio API', '12 - Infinite Scroll Posts  Fetch, AsyncAwait, CSS Loader', '13 - Typing Game  DOM, Intervals, Events', '14 - Speech Text Reader  Speech Synthesis', '15 - Memory Cards  CSS Effects, Local Storage', '16 - Lyrics Search App  Fetch, Pagination, Lyrics.ovh API', '17 - Relaxer App  CSS Animations, setTimeout', '18 - Breakout Game  HTML5 Canvas API', '19 - New Year Countdown  DOM, Date & Time', '2 - Form Validator  Intro Project', '20 - Sortable List  Drag & Drop API', '21 - Speak Number Guessing Game  Speech Recognition', '3 - Movie Seat Booking  DOM & Local Storage', '4 - Custom Video Player  HTML5 Video API', '5 - Exchange Rate Calculator  Fetch & JSON Intro', 
+'6 - DOM Array Methods  forEach, map, filter, sort, reduce', '7 - Menu Slider & Modal  DOM & CSS', '8 - Hangman Game  DOM, SVG, Events', '9 - Meal Finder  Fetch & MealDB API']
+
+split_char_list = [".", " ", " -"]
+
 def print_list(lst):
   for item in lst:
     print(item)
@@ -10,16 +18,24 @@ def print_list(lst):
 # def sort(lst):
 #   return sorted(lst, key=lambda x: int(x[0]))
 
+# return an element of the list if it exists in a string
+def find_element_in_string(lst, string):
+  for item in lst:
+    if item in string:
+      return item
+  return None
+
 def sort_list(unsorted_list):
+  split_char = find_element_in_string(split_char_list, unsorted_list[0])
   lst = []
   sorted_list = []
   for item in unsorted_list:
-    item_num, item_name = item.split('.', 1)
+    item_num, item_name = item.split(split_char, 1)
     lst.append([item_num, item_name])
 
   lst = sorted(lst, key=lambda x: int(x[0]))
   for item in lst:
-    sorted_list.append(item[0]+"."+item[1])
+    sorted_list.append(item[0]+split_char+item[1])
   
   return sorted_list
 
@@ -39,11 +55,19 @@ def sort_list(unsorted_list):
 # # sorted_lst = sort(lst)
 # print("***************** sorted *****************")
 # print_list(sort_list(lst_to_sort))
+  # (r"(^[0-9]+\.)+", re_srt  
   
+
 re_srt = "0. Introduction"
-if re.match(r"(^[0-9]+\.)+", re_srt):
-  print("Yes")
-else: print("No")
+if re.match(r"(^[0-9]+(\.|-| ))+", re_srt):
+  print("matched")
+else:
+  print("not matched")
+
+# print_list(sort_list(lst_to_sort, "."))
+# print_list(sort_list(lst_to_sort2, " "))
+print_list(sort_list(lst_to_sort))
+
 
 
   
